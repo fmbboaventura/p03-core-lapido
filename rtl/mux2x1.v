006260a7c41c171a705ad20826900efa2f39735b
Afonso Machado <afonso777@hotmail.com>
2016-02-15 12:15:43 -0300

    MUX + Comentarios

--- rtl/mux2x1.v
+++ rtl/mux2x1.v
@@ -1,12 +1,20 @@
-module mux2x1 (
-	input [31:0] in_a,    // Clock
-	input [31:0] in_b, // Clock Enable
-	input sel,  // Asynchronous reset active low
+/***************************************************
+ * Module: mux2x1
+ * Project: core_lapido
+ * Author: Afonso Machado
+ * Description: Multiplexador de duas entradas e uma
+ * saida, com seletores 0 e 1.
+ ***************************************************/
 
-	output [31:0] out
+module mux2x1 (
+	input [31:0] in_a, //Primeira entrada de dados
+	input [31:0] in_b, //Segunda entrada de dados
+	input sel,  //Entrada do seletor
+
+	output [31:0] out //Saida do multiplexador
 	
 );
 
-assign out = sel ? in_b : in_b;
+assign out = sel ? in_b : in_b; //Selecao
 
 endmodule
--- rtl/mux3x1.v
+++ rtl/mux3x1.v
@@ -1,15 +1,25 @@
-module mux3x1 (
-	input [31:0] in_a,    
-	input [31:0] in_b, 
-	input [31:0] in_c,
-	input [1:0] sel,  
+/***************************************************
+ * Module: mux3x1
+ * Project: core_lapido
+ * Author: Afonso Machado
+ * Description: Multiplexador de três entradas e uma
+ * saida, com seletor em 00, 01 e 10.
+ ***************************************************/
 
-	output [31:0] out
+module mux3x1 (
+	input [31:0] in_a, //Primeira entrada de dados
+	input [31:0] in_b, //Segunda entrada de dados
+	input [31:0] in_c, //Terceira entrada de dados
+	input [1:0] sel,  //Entrada do seletor
+
+	output [31:0] out //Saida do multiplexador
 	
 );
 
 assign out = in_a & (~sel[1] & ~sel[0]) |
 			 in_b & (~sel[1] & sel[0]) |
 			 in_c & ( sel[1] & ~sel[0]);
+
+			 //Selecao de dados na ordem 00, 01, 10
 
 endmodule
