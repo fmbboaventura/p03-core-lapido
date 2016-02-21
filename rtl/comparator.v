be985cfed839339841a5f016e2ee5be80bd66e95
Gadiel Xavier <gadiel.xavier@gmail.com>
2016-02-21 00:27:43 -0300

    Adicionado header no arquivo comparator.v

--- rtl/comparator.v
+++ rtl/comparator.v
@@ -1,3 +1,12 @@
+/***************************************************
+ * Module: comparator
+ * Project: core_lapido
+ * Author: Gadiel Xavier
+ * Description: Compara dois numeros de 32 bits e a 
+saida eh um numero de 1 bit, sendo que 1 representa verdadeiro 
+e 0 falso.
+ ***************************************************/
+
 module comparator (
 	input [31:0] a,
 	input [31:0] b,
