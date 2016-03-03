956012d8b34fc0bb4176e4db2288f951e9820d40
Filipe Boaventura <fmbboaventura@gmail.com>
2016-03-03 03:56:00 -0300

    Instancias dos 5 estágios no top level.

--- rtl/lapido_top.v
+++ rtl/lapido_top.v
@@ -126,4 +126,116 @@ module lapido_top (input clk, input rst);
         .wb_res_mux(ID_EX_wb_res_mux), // Seleciona o dado que sera escrito no registrador
         .reg_write_enable(ID_EX_reg_write_enable) // Habilita a escrita no banco de registradores
     );
+
+    EX_stage EX_stage_inst
+    (
+        // Entradas
+        .clk(clk),
+        .rst(rst),
+        .alu_src_mux(ID_EX_alu_src_mux),
+        .alu_funct(ID_EX_alu_funct),
+        .reg_dst_mux(ID_EX_reg_dst_mux),
+        .sel_j_jr(ID_EX_sel_j_jr),
+        .in_next_pc(ID_EX_out_next_pc),
+        .in_immediate(ID_EX_imm),
+        .in_data_rs(ID_EX_rs_data),
+        .in_data_rt(ID_EX_rt_data),
+        .in_rs(ID_EX_flag_addr),
+        .in_rt(ID_EX_rt),
+        .in_rd(ID_EX_rd),
+        .in_sel_jflag_branch(ID_EX_sel_jflag_branch),
+        .in_mem_write_enable(ID_EX_mem_write_enable),
+        .in_sel_beq_bne(ID_EX_sel_beq_bne),
+        .in_fl_write_enable(ID_EX_fl_write_enable),
+        .in_sel_jt_jf(ID_EX_sel_jt_jf),
+        .in_is_branch(ID_EX_is_branch),
+        .in_is_jump(ID_EX_is_jump),
+        .in_wb_res_mux(ID_EX_wb_res_mux),
+        .in_reg_write_enable(ID_EX_reg_write_enable),
+
+        // Saidas
+        .out_sel_jflag_branch(EX_MEM_out_sel_jflag_branch),
+        .out_mem_write_enable(EX_MEM_out_mem_write_enable),
+        .out_sel_beq_bne(EX_MEM_out_sel_beq_bne),
+        .out_fl_write_enable(EX_MEM_out_fl_write_enable),
+        .out_sel_jt_jf(EX_MEM_out_sel_jt_jf),
+        .out_is_branch(EX_MEM_out_is_branch),
+        .out_is_jump(EX_MEM_out_is_jump),
+        .out_wb_res_mux(EX_MEM_out_wb_res_mux),
+        .out_reg_write_enable(EX_MEM_out_reg_write_enable),
+        .out_next_pc(EX_MEM_out_next_pc),
+        .out_branch_addr(EX_MEM_out_branch_addr),
+        .out_immediate(EX_MEM_out_immediate),
+        .abs_addr(EX_MEM_abs_addr),
+        .mem_addr(EX_MEM_mem_addr),
+        .mem_data(EX_MEM_mem_data),
+        .alu_out(EX_MEM_alu_out),
+        .alu_flags_out(EX_MEM_alu_flags_out),
+        .flag_addr(EX_MEM_flag_addr),
+        .reg_dst(EX_MEM_reg_dst)
+    );
+
+    MEM_stage MEM_stage_inst
+    (
+        // Entradas
+        .clk(clk),
+        .rst(rst),
+        .sel_jflag_branch(EX_MEM_out_sel_jflag_branch),
+        .in_mem_write_enable(EX_MEM_out_mem_write_enable),
+        .in_sel_beq_bne(EX_MEM_out_sel_beq_bne),
+        .in_fl_write_enable(EX_MEM_out_fl_write_enable),
+        .in_sel_jt_jf(EX_MEM_out_sel_jt_jf),
+        .in_is_branch(EX_MEM_out_is_branch),
+        .in_is_jump(EX_MEM_out_is_jump),
+        .in_wb_res_mux(EX_MEM_out_wb_res_mux),
+        .in_reg_write_enable(EX_MEM_out_reg_write_enable),
+        .in_next_pc(EX_MEM_out_next_pc),
+        .in_branch_addr(EX_MEM_out_branch_addr),
+        .in_immediate(EX_MEM_out_immediate),
+        .abs_addr(EX_MEM_abs_addr),
+        .in_mem_addr(EX_MEM_mem_addr),
+        .in_mem_data(EX_MEM_mem_data),
+        .in_alu_res(EX_MEM_alu_out),
+        .alu_flags(EX_MEM_alu_flags_out),
+        .flag_addr(EX_MEM_flag_addr),
+        .in_reg_dst(EX_MEM_reg_dst),
+
+        //saidas
+        .out_is_jump(MEM_WB_out_is_jump),//
+        .out_branch_taken(MEM_WB_out_branch_taken),//
+        .out_branch_addr(MEM_WB_out_branch_addr),
+        .out_reg_write_enable(MEM_WB_out_reg_write_enable),//
+        .out_wb_res_mux(MEM_WB_out_wb_res_mux), //
+        .out_next_pc(MEM_WB_out_next_pc), // o pc + 1 do jal
+        .out_mem_data(MEM_WB_out_mem_data),//
+        .out_alu_res(MEM_WB_out_alu_res),//
+        .out_reg_dst(MEM_WB_out_reg_dst),//
+        .imm(MEM_WB_imm),//
+        .out_jump_addr(MEM_WB_out_jump_addr)//
+    );
+
+    WB_stage WB_stage_inst
+    (
+        // Entradas
+        .in_is_jump(MEM_WB_out_is_jump),//
+        .in_branch_taken(MEM_WB_out_branch_taken),//
+        .in_branch_addr(MEM_WB_out_branch_addr),
+        .in_reg_write_enable(MEM_WB_out_reg_write_enable),//
+        .wb_res_mux(MEM_WB_out_wb_res_mux), //
+        .next_pc(MEM_WB_out_next_pc), // o pc + 1 do jal
+        .mem_data(MEM_WB_out_mem_data),//
+        .alu_res(MEM_WB_out_alu_res),//
+        .in_reg_dst(MEM_WB_out_reg_dst),//
+        .imm(MEM_WB_imm),//
+        .in_jump_addr(MEM_WB_out_jump_addr),//
+
+        // Saidas
+        .out_is_jump(WB_IF_out_is_jump),
+        .out_branch_taken(WB_IF_out_branch_taken),
+        .out_branch_addr(WB_IF_out_jump_addr),
+        .out_reg_write_enable(WB_ID_out_reg_write_enable),
+        .wb_res(WB_ID_wb_res),
+        .out_reg_dst(WB_ID_out_reg_dst),
+        .out_jump_addr(WB_IF_out_jump_addr)
+    );
 endmodule // lapido_top
